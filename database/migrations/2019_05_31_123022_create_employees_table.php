2b66ea2815dd94d7d16febbfd5c81eb04f4513d7
evan4 <evan4mc@gmail.com>
2019-05-31 17:31:09 +0500

    create migrations

---
+++ database/migrations/2019_05_31_123022_create_employees_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->unsignedInteger('position_id');
+            $table->unsignedInteger('salary');
+            $table->timestamps();
+
+            $table->foreign('position_id')->references('id')->on('positions')
+                ->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
