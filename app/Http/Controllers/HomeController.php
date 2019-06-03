f52aa615ecb8b746a2dba7c566f3491d55c01f2b
evan4 <evan4mc@gmail.com>
2019-06-03 10:57:45 +0500

    add laravel-debugbar

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Company;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $companoes = new Company();
+        return view('home', compact('companoes'));
     }
 }
